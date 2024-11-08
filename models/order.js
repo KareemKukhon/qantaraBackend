32997a64cc382f9305fb06a1b0b435e7ffdd2508
KareemKukhon <kareemkukhon1@gmail.com>
2024-11-09 00:47:27 +0200

    add order

--- models/order.js
+++ models/order.js
@@ -5,26 +5,22 @@ const orderSchema = new mongoose.Schema({
         type: String, 
         enum: ['pending', 'completed', 'cancelled'], // حالة الطلب
         default: 'pending',
-        required: true 
     },
     orderPrice: { type: Number, }, // سعر الطلب
     orderDate: { type: Date, default: Date.now }, // تاريخ الطلب
     productId: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Product', 
-        required: true 
     }, // id القطعه
     userId: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User', 
-        required: true 
     }, // user id
     carId: { 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Car', 
-        required: true 
     }, // car id
-    warrantyDuration: { type: String, required: true }, // مدة الضمان
+    warrantyDuration: { type: String }, // مدة الضمان
     images: [{ type: String, trim: true }], // الصور, array of image URLs
     color: { type: String, trim: true }, // اللون
     notes: { type: String, trim: true }, // الملاحظات
